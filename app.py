7596a36ec0563246ee74e99b210d199ba5c411ac
@jikjeong <138367697+jikang-jeong@users.noreply.github.com>
2026-03-17 13:20:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ import random
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    if random.random() < 0.9:
-        return "Internal Server Error", 500
-    hostname = socket.gethostname()[:12]
+def hello(): 
     return render_template("index.html", message=f"CloudWatch alarm test new - Task: {hostname}")
 
 @app.route("/health")
