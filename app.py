587188614c94023aa90e6faf267de40886fc6202
@jikjeong <138367697+jikang-jeong@users.noreply.github.com>
2026-03-17 14:13:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template
 import os
 import socket
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    hostname = socket.gethostname()[:12]  
-    return render_template("index.html", message=f" 1406 CI/CD test - [current env]  Task: {hostname}")
+    if random.random() < 0.8:
+        return "Internal Server Error", 500
+    hostname = socket.gethostname()[:12]
+    return render_template("index.html", message=f"CloudWatch alarm test - Task: {hostname}")
 
 @app.route("/health")
 def health():
-    return "SUCCESS", 200
-
-@app.route("/error")
-def error():
-    return "Internal Server Error", 500
+    return "SUCCESS", 200  # 헬스체크는 통과 (배포 성공 → Bake Time 진입)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
